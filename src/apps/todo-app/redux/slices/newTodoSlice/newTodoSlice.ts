6d6158f39e00e2f3597a7af2722b8387c3707a3a
Protonm0KEN <109662775+Protonm0KEN@users.noreply.github.com>
2024-07-30 21:29:48 +0500

    Created newTodoSlice actions

--- src/apps/todo-app/redux/slices/newTodoSlice/newTodoSlice.ts
+++ src/apps/todo-app/redux/slices/newTodoSlice/newTodoSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { newTodoSliceInitialState } from "./types/newTodoSliceTypes";
 
 const DateObj = new Date()
@@ -19,7 +19,54 @@ const newTodoSlice = createSlice({
     initialState: initialState,
     reducers: {
 
+        //newTodo general
+        setNewTodoName: (state, action: PayloadAction<string>) => {
+            state.newTodoName = action.payload
+        },
+        setNewTodoContent: (state, action: PayloadAction<string>) => {
+            state.newTodoContent = action.payload
+        },
+        updateNewTodoId: (state, action: PayloadAction<string>) => {
+            state.newTodoId = action.payload
+        },
+
+        //newTodo time
+        //Updating time after creation a newTodo
+        updateNewTodoTimeOfCreation: (state, action: PayloadAction<string>) => {
+            state.newTodoTimeOfCreation = action.payload
+        },
+        updateNewTodoDateOfCreation: (state, action: PayloadAction<string>) => {
+            state.newTodoDateOfCreation = action.payload
+        },
+        //setting Finishing times
+        setNewTodoDateOfFinishing: (state, action: PayloadAction<string>) => {
+            state.newTodoDateOfFinishing = action.payload
+        },
+        setNewTodoTimeOfFinishing: (state, action: PayloadAction<string>) => {
+            state.newTodoTimeOfFinishing = action.payload
+        },
+
+        //newTodo group
+        setNewTodoGroupName: (state, action: PayloadAction<string>) => {
+            state.newTodoGroupName = action.payload
+        },
+        setNewTodoGroupId: (state, action: PayloadAction<string>) => {
+            state.newTodoGroupId = action.payload
+        }
     }
 })
-export const {} = newTodoSlice.actions
+export const { 
+
+    setNewTodoName,
+    setNewTodoContent,
+    updateNewTodoId,
+
+    updateNewTodoTimeOfCreation,
+    updateNewTodoDateOfCreation,
+    setNewTodoDateOfFinishing,
+    setNewTodoTimeOfFinishing,
+
+    setNewTodoGroupName,
+    setNewTodoGroupId
+} = newTodoSlice.actions
 export default newTodoSlice.reducer
